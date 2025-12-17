c338d9d21a4ff1ffe4ab53175575aa354ff51cee
奎京 森岡 <pi.xxx.nk@gmail.com>
2025-12-17 13:20:25 +0900

    プロジェクトファイルを追加。

---
+++ 課題2.22-④/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+interface IWorkable
+{
+    void Work();
+}
+interface IEatable
+{
+    void Eat();
+}
+class Robot : IWorkable
+{
+    public void Work()
+    {
+        Console.WriteLine("[Robot]:働く");
+    }
+}
+
+class Human : IWorkable, IEatable
+{
+    public void Work()
+    {
+        Console.WriteLine("[Human]:働く");
+    }
+    public void Eat()
+    {
+        Console.WriteLine("[Human]:食べる");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        IWorkable robot = new Robot();
+        robot.Work();
+
+        IWorkable humanWorker = new Human();
+        humanWorker.Work();
+
+        IEatable humanEater = new Human();
+        humanEater.Eat();
+    }
+}
